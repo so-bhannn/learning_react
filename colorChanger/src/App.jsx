5a7c10e06787da9995bf8b695ddb2ddd8995e054
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-01-14 23:56:11 +0530

    update colorchanger

--- colorChanger/src/App.jsx
+++ colorChanger/src/App.jsx
@@ -4,11 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setColor] = useState(0)
 
   return (
     <>
-      <div className='bg-red w-full h-full'/>
+      <div className='bg-red-500 text-blue-200 w-full h-screen'>
+        <div className="flex flex-wrap fixed bottom-12 justify-center inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center"></div>
+          <button>red</button>
+          <button>green</button>
+          <button>blue</button>
+        </div>
+      </div>
     </>
   )
 }
